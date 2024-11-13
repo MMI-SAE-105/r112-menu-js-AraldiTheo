e2f1266d1a2fd993197c6d42184e63623a5f9141
Araldi Théo <theo.araldi@edu.univ-fcomte.fr>
2024-11-13 11:34:55 +0100

    classe noscroll.

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
 const nav = document.querySelector('.menu');
 const toggle = document.querySelector('.menu-btn');
+const body = document.querySelector('body')
+
 toggle.addEventListener("click" ,() =>{
     const isOpen = toggle.ariaExpanded === "true";
 const isClosed = !isOpen;
 console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+
     nav.ariaHidden = isOpen;
     toggle.ariaExpanded = isClosed;
+    body.classList.toggle('noscroll');
 })
