5164af31d983f02fcfbe9a0ed4212b45ff747fa7
Araldi Théo <theo.araldi@edu.univ-fcomte.fr>
2024-11-20 16:27:28 +0100

    Chat 1 et 2

--- src/js/script.js
+++ src/js/script.js
@@ -9,5 +9,5 @@ console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
 
     nav.ariaHidden = isOpen;
     toggle.ariaExpanded = isClosed;
-    body.classList.toggle('noscroll');
+    body.classList.toggle('noscroll', isClosed);
 })
